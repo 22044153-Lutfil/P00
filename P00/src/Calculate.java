ed78a86d0979d4d16c730abb8014f9e75cf1d6f2
22044153 <22044153@10.172.162.190>
2023-05-24 12:49:43 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculate.java
@@ -0,0 +1,23 @@
+
+public class Calculate {
+
+public int add (int a, int b) {
+	 return a + b;
+	}
+
+public int substract (int a, int b) {
+	 return a - b;
+	}
+
+public int multiply (int a, int b) {
+	 return a * b;
+	}
+
+public int divide (int a, int b) {
+	if (b == 0) {
+		throw new IllegalArgumentException();
+	}
+	return a / b;
+}
+
+}
---
+++ P00/test/CalculateTest.java
@@ -0,0 +1,77 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculateTest {
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		// Prepare the testing datas
+	}
+
+
+	@Test
+	public void testAdd() {
+		int a = 555;
+		int b = 111;
+			
+		Calculate cal = new Calculate();
+		int actual = cal.add(a, b); 
+			 
+		int expected = 666;
+		assertEquals (expected, actual);
+		}
+
+	
+	public void testSubstract () {
+		int a = 50;
+		int b = 30;
+			
+		Calculate cal = new Calculate();
+		int actual = cal.substract (a, b); 
+			 
+		int expected = 20;
+		assertEquals (expected, actual);
+		}
+	
+	public void testMultiply() {
+		int a = 111;
+		int b = 9;
+			
+		Calculate cal = new Calculate();
+		int actual = cal.add(a, b); 
+			 
+		int expected = 999;
+		assertEquals (expected, actual);
+		}
+
+	public void testDivide () {
+		int a = 369;
+		int b = 3;
+			
+		Calculate cal = new Calculate();
+		int actual = cal. divide (a, b); 
+			 
+		int expected = 123;
+		assertEquals (expected, actual);
+		}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+}
